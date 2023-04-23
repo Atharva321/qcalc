93950145b1d9e7b0b6b3ccafc58eaf865279ae7f
Atharva Patil <44301928+Atharva321@users.noreply.github.com>
2023-04-23 22:18:34 +0530

    Update QcalcApplication.java

--- main/QcalcApplication.java
+++ main/QcalcApplication.java
@@ -1,1 +1,23 @@
+package com.crio.qcalc;
 
+// import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcalcApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(QcalcApplication.class, args);
+		// System.out.println("Finally Code is Running.........");
+		StandardCalculator calc = new StandardCalculator();
+		calc.add(18, 9);
+		System.out.println(calc.getResult());
+		calc.subtract(18,9);
+		System.out.println(calc.getResult());
+		calc.multiply(18, 9);
+		System.out.println(calc.getResult());
+		calc.divide(18, 9);
+		System.out.println(calc.getResult());
+	}
+
+}
